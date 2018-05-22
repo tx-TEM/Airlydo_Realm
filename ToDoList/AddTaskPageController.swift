d2d32e75eaf70a8c3222654f5abe144d0c84e28b
highhigheggman <highhigheggman@yahoo.c.jp>
2018-05-22 22:43:22 +0900

    update

--- ToDoList/AddTaskPageController.swift
+++ ToDoList/AddTaskPageController.swift
@@ -10,43 +10,88 @@ import UIKit
 import Eureka
 
 class AddTaskPageController: FormViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        form +++ Section("タスク")
+        form +++ Section("Task")
             <<< TextRow("TextFiled"){
-                $0.title = "タイトル"
-                $0.placeholder = "ここに書いてね"
+                $0.title = "Add a Task"
             }
-            <<< TextRow(){
-                $0.title = "詳細"
-                $0.placeholder = "ここに書いてね"
+            <<< LabelRow("areaTag"){
+                $0.title = "Note"
+                $0.value = "Note"
+                }.onCellSelection{ cell, row in
+                    cell.backgroundColor = .lightGray
             }
-            <<< DateRow() { $0.title = "期限"; $0.value = Date()}
+            
+            
+            
+            <<< TextRow("aaa").cellSetup({ (cell, row) in
+                cell.height = ({return 10})
+            })
+        
+            <<< ActionSheetRow<String>("listTag") {
+                $0.title = "List"
+                $0.selectorTitle = "List"
+                $0.options = ["なし"]
+                $0.value = "なし"
+                }.onChange{row in
+                    print(row.value as Any)
+        }
+        
+        
+        form +++ Section("")
+            <<< DateRow() {
+                $0.title = "Due Date"
+                $0.value = Date()
+                
+            }
+            
+            <<< ActionSheetRow<String>("repeatTag") {
+                $0.title = "Repeat"
+                $0.selectorTitle = "繰り返し"
+                $0.options = ["毎週","毎月","毎年", "なし"]
+                $0.value = "なし"
+                }.onChange{row in
+                    print(row.value as Any)
+                }
+        
 
         
         form +++ MultivaluedSection(multivaluedOptions: [.Reorder, .Insert, .Delete],
-                           header: "リマインダー") {
+                           header: "Reminder") {
                             $0.addButtonProvider = { section in
                                 return ButtonRow(){
-                                    $0.title = "追加"
+                                    $0.title = "Add"
                                 }
                             }
                             $0.multivaluedRowToInsertAt = { index in
                                 return DateTimeRow() {
-                                    $0.title = "通知時刻"
+                                    $0.title = ""
                                 }
-                            }
-                            $0 <<< DateTimeRow() {
-                                $0.title = "通知時刻"
                             }
         }
         
-        
-        
-        
-        
+        form +++ Section("Option")
+            <<< ActionSheetRow<String>("") {
+                $0.title = "Priority"
+                $0.selectorTitle = "set priority"
+                $0.options = ["High","Middle","Low"]
+                $0.value = "Middle"
+                }.onChange{row in
+                    print(row.value as Any)
+            }
+            <<< ActionSheetRow<String>("assignTag") {
+                $0.title = "Assign"
+                $0.selectorTitle = "Assign"
+                $0.options = ["自分"]
+                $0.value = "自分"
+                }.onChange{row in
+                    print(row.value as Any)
+            }
+
         
         // Do any additional setup after loading the view.
     }
